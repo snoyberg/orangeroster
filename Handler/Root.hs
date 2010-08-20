1cc05d7e5f03a685b7e3f35a54adc252d14b09f8
Michael Snoyman <michael@snoyman.com>
2010-08-20 17:38:41 +0300

    Layout changes, logo, homepage, etc

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -6,9 +6,12 @@ import Yesod
 import Yesod.Helpers.Auth
 import App
 import Settings
+import StaticFiles
 
 getRootR :: Handler OR RepHtml
 getRootR = do
     applyLayoutW $ do
         setTitle "OrangeRoster Homepage"
         addBody $(hamletFile "root")
+        addStyle $(cassiusFile "root")
+        addJavascript $(juliusFile "root")
